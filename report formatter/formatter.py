d95151aafd55494a3e7b00bdc2bb3f4de95d3adf
Hedwig <mme94@scarletmail.rutgers.edu>
2017-08-10 16:16:17 -0400

    need a way to fit the text to each column

--- report formatter/formatter.py
+++ report formatter/formatter.py
@@ -4,7 +4,8 @@ import csv
 from openpyxl import Workbook
 from openpyxl.styles import Border, Side, PatternFill,\
                             Font, GradientFill, Alignment
-
+from openpyxl.worksheet.dimensions import ColumnDimension
+                            
 # this useful function taken from konrad
 # at "https://gist.github.com/konrad/4154786"
 def _convert_to_number(cell):
@@ -30,23 +31,34 @@ def csv_to_xlsx(input_path):
     
 # Taken from documentation   
 def style_range(ws, first_cell, last_cell):
-    # format first row font
-    for c in range(first_cell.col_idx, last_cell.col_idx+1):
-        cell = ws.cell(row = first_cell.row, column = c)
-        ft = Font(name='Calibri', bold=True)
-        al = Alignment(horizontal="center", vertical="center")
-        cell.font = ft
-        cell.alignment = al
-        
     # colouring rows after first row
     for r in range(first_cell.row, last_cell.row+1):
         for c in range(first_cell.col_idx, last_cell.col_idx+1): 
             # colour odd numbered rows
+            if r == first_cell.row:
+                ft = Font(name='Courier', bold=True)
+            else:
+                ft = Font(name='Courier')
+            cell = ws.cell(row = r, column = c)
+            al = Alignment(horizontal="center", vertical="center")
+            cell.alignment = al
+            cell.font = ft
             if r % 2 == 1:
-                cell = ws.cell(row = r, column = c)
                 fill = PatternFill('solid', fgColor="DDDDDD")
                 cell.fill = fill
+                
+    # fit text to column
+    column_widths = []
+    for row in ws:
+        for i, cell in enumerate(row):
+            if len(column_widths) > i:
+                if len(str(cell.value)) > column_widths[i]:
+                    column_widths[i] = len(str(cell.value))
+            else:
+                column_widths.append(len(str(cell.value)))
 
+    for i, column_width in enumerate(column_widths):
+        ws.column_dimensions[ws.cell(row=1, column=i+1).column].width = column_width
 
                 
 if __name__ == "__main__":
